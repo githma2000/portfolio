7882572ec55c8dfeeb17fa666734253cfec7e54d
githma2000 <137407122+githma2000@users.noreply.github.com>
2024-06-26 11:47:16 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,58 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/GithmaProfile.jpg";
+import { motion } from "framer-motion"
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0},
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay}
+  },
+});
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-8 text-6xl font-thin tracking-light lg:mt-18 lg:text-7xl">
+              Githma Manthinie
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+             className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 
+            bg-clip-text text-4xl tracking-tight text-transparent">
+              Front End Developer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible" 
+              className="my-2 max-w-xl py-8 font-light tracking-tighter">{HERO_CONTENT}
+            </motion.p>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <motion.img 
+            initial={{ x: 100, opacity: 0}}
+            animate={{ x: 0, opacity: 1}}
+            transition={{ duration: 1, delay: 1.2}}
+            src={profilePic}
+            alt="Githma Manthinie"
+            className="w-80 h-100  rounded-tl-3xl rounded-br-3xl shadow-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,68 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Hello! I'm Githma Manthinie, a passionate and dedicated front-end developer
+ with a strong focus on creating stunning, user-friendly web interfaces. My journey in web development
+  began with a fascination for how design and functionality can come together to create seamless user
+ experiences. Over the years, I've honed my skills in various technologies and frameworks to bring ideas to life on the web.`;
+export const ABOUT_TEXT = `I am a passionate and dedicated Front End Developer. With a keen eye for design
+ and a strong technical foundation, I specialize in creating visually appealing and highly functional web applications. My journey 
+ in web development began with a fascination for how websites work, and over the years, I've honed my skills to transform creative 
+ ideas into digital reality.I thrive on challenges and enjoy solving complex problems with elegant solutions. My expertise includes
+  HTML, CSS, JavaScript, and modern frameworks like React. I am constantly learning and staying up-to-date with the latest industry 
+  trends to ensure that my work remains at the cutting edge of web development.`;
+export const EXPERIENCES = [
+  {
+    year: "Feb 2020 - Nov 2020",
+    role: "Internship trainee",
+    company: "Sampath Bank.PLC",
+    description: `As an intern at Sampath Bank PLC in the NSC department, I excelled in data entry and cheque clearance, ensuring accurate records and timely transactions. Demonstrated punctuality, effective teamwork, and consistently met deadlines, contributing to smooth departmental operations.`,
+    technologies: ["Bank Management System"],
+  },
+  {
+    year: "July 2023 - Jan 2024",
+    role: "Trainee Quality Assurance Engineer",
+    company: "EchonLabs.Pvt.Ltd",
+    description: `As a Trainee Quality Assurance Engineer at EchonLabs Pvt. Ltd., I specialized in Manual Testing, API Testing, and Test Case Writing. I gained practical experience in web revamping for internal projects and contributed to the BaseNeo project, enhancing my skills in quality assurance and project delivery.`,
+    technologies: ["TestLink", "Postman"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Weather App",
+    image: project1,
+    description:
+      "A sleek, responsive weather forecasting app built with React and Vite, leveraging the OpenWeatherMap API for real-time weather data. It delivers accurate, up-to-date information with a modern, user-friendly interface.",
+    technologies: ["HTML", "CSS", "React"],
+  },
+  {
+    title: "To Do List",
+    image: project2,
+    description:
+      "Introducing my TodoList App: a streamlined tool for managing tasks with ease. Leveraging basic CRUD operations, it enables users to create, read, update, and delete tasks efficiently, enhancing productivity and organization in everyday life.",
+    technologies: ["React", "MongoDB", "Express", "NodeJS"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "Explore my React portfolio website crafted with Framer Motion for stunning animations and Tailwind CSS for sleek designs. Discover a showcase of projects, featuring responsive layouts and interactive elements that highlight my skills and creativity in web development.",
+    technologies: ["React", "Framer Motion", "TailwindCSS"],
+  },
+  {
+    title: "Hospital Management System",
+    image: project4,
+    description:
+      "Experience efficiency in healthcare with my C-based Hospital Management System. This project optimizes hospital operations through robust management of patient records, appointments, and staff administration, ensuring streamlined workflows and improved patient care delivery.",
+    technologies: ["C"],
+  },
+];
+
+export const CONTACT = {
+  address: "293/1, yahampath Mawatha, Godigamuwa, Mahargama ",
+  phoneNo: "+94772282726",
+  email: "githma00@gmail.com",
+};
