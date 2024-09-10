73f10fce73de28dc5f5d132cf3f1d0958e4e7257
githma2000 <137407122+githma2000@users.noreply.github.com>
2024-09-10 10:48:33 +0530

    new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
              whileInView={{ opacity: 1, x:0 }}
              initial={{ opacity: 0, x: -100}}
              transition={{ duration: 0.5}}
-             className="w-full lg:w-1/2 lg:p-8">
+             className="w-full lg:w-1/2 lg:p-10">
                 <div className="flex items-center justify-center">
                     <img  className=" rounded-2xl lg:mr-8"src={aboutImg} alt="about"/>
                 </div>
